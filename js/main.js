e71adb5221e4863f27aed152a555fecb30fcd3b8
evox1994 <dmitrybatukhtin@gmail.com>
2020-09-28 16:38:42 +0700

    sdvdsfv

---
+++ js/main.js
@@ -0,0 +1,138 @@
+$(document).ready(function(){
+
+	$(document).on('click','.radio-btn',function(){
+		if ( $(this).hasClass('active') ) {
+			$(this).removeClass('active');
+		} else {
+			$(this).removeClass('error');
+			$(this).addClass('active');
+		}
+	});
+
+	$(document).on('click','.mobile-btn',function(){
+		if ( $(this).hasClass('active') ){
+			$(this).removeClass('active');
+			$('.mobile-menu').removeClass('active');
+			$('body').removeClass('no-scroll');
+			$('li.li-drop').removeClass('active');
+		} else {
+			$(this).addClass('active');
+			$('.mobile-menu').addClass('active');
+			$('body').addClass('no-scroll');
+		}
+	});
+
+	$('.fancybox-gal').fancybox({loop: true});
+	$('.fancybox').fancybox({touch: false});
+	$('input[type="tel"]').inputmask('+7 (999) 999-99-99');
+
+	$(document).on('click','.close-btn',function(){
+		$('.mobile-btn').removeClass('active');
+		$('.mobile-menu').removeClass('active');
+		$('body').removeClass('no-scroll');
+		$('.mobile-menu .nav li.li-drop').removeClass('active');
+		$('li.li-drop').removeClass('active');
+	});
+
+	$('input').on('input',function(){
+		$(this).removeClass('error');
+	});
+	$('textarea').on('input',function(){
+		$(this).removeClass('error');
+	});
+
+	$('form').on('submit',function(){
+		var valid = true;
+
+		if ( $(this).find('.policy-text .radio-btn').length ){
+			if ( $(this).find('.policy-text .radio-btn').hasClass('active') ){
+				$(this).find('input').each(function(){
+					if(!$(this).val().length) { 
+						event.preventDefault();
+						valid = false;
+						$(this).addClass("error"); 
+					} else { 
+						$(this).removeClass("error"); 
+					}
+				});
+				$(this).find('textarea').each(function(){
+					if(!$(this).val().length) { 
+						event.preventDefault();
+						valid = false;
+						$(this).addClass("error"); 
+					} else { 
+						$(this).removeClass("error"); 
+					}
+				});
+			} else {
+				$(this).find('.policy-text .radio-btn').addClass('error');
+				event.preventDefault();
+				return false;
+			}
+		} else {
+			$(this).find('input').each(function(){
+				if(!$(this).val().length) { 
+					event.preventDefault();
+					valid = false;
+					$(this).addClass("error"); 
+				} else { 
+					$(this).removeClass("error"); 
+				}
+			});
+			$(this).find('textarea').each(function(){
+				if(!$(this).val().length) { 
+					event.preventDefault();
+					valid = false;
+					$(this).addClass("error"); 
+				} else { 
+					$(this).removeClass("error"); 
+				}
+			});
+		}
+
+		if (!valid) {
+			event.preventDefault();
+			return false;
+		}
+	});
+
+	function footerYear(){
+		var date = new Date();
+		var year = date.getFullYear();
+		$('#footer-year').text(year);
+	}
+	footerYear();
+
+	$('.action-slider').slick({
+		dots: true
+	});
+
+	$('.b-catalog-cat .slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1230,
+				settings: {
+					slidesToShow: 3
+				}
+			}
+		]
+	});
+
+	$(document).on('click','.nav-list li.li-drop span',function(){
+		$(this).parent('li.li-drop').addClass('active');
+	});
+
+	$(document).on('click','.nav-list .back-btn',function(){
+		$(this).parent('.drop').parent('li.li-drop').removeClass('active');
+	});
+
+	$(document).on('click','.swap-list > li',function(){
+		if ( !$(this).hasClass('active') ){
+			$(this).parents('.swap-list').find('li').removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+
+});
